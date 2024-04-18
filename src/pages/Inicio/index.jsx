f94eee447104a93cca347798a0857d42ee1ea469
VillyRosa <villyrosa@hotmail.com>
2024-04-18 17:59:52 -0300

    first commit

---
+++ src/pages/Inicio/index.jsx
@@ -0,0 +1,14 @@
+// import styles from './Inicio.module.css';
+import Cabecalho from 'componentes/cabecalho';
+import Rodape from 'componentes/Rodape';
+
+function Inicio() {
+  return (
+    <>
+      <Cabecalho />
+      <Rodape />
+    </>
+  );
+}
+
+export default Inicio;
